79528fc32aa488243bf6e770c387857349648e26
Humam <humamhusameldeen.github@gmail.com>
2024-05-31 15:51:14 +0300

    Fixing "No open ports detected"

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
 
   app.use('/', swaggerUi.serve, swaggerUi.setup(document));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
